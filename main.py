603f2569185992e6feab09e447329eb94907f5ca
morgane-rossi <morgane.rossi@laplateforme.io>
2025-03-18 09:52:34 +0100

    basic graphic interface

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-#from crud.crud_cat import Category
+from crud.crud_cat import Category
 from crud.crud_prod import Product
+from crud.reuh import display_allproducts
+from graphics.window import StoreWindow
+from tkinter import *
 
-prod = Product("plop", 0, 0, 0)
-prod.display_allproducts()
+main_window = Tk()
+
+custom_display = Label(main_window, text="Application inventaire", font=("Helvetica, 18"))
+
+custom_display.pack()
+
+rows = display_allproducts()
+for r in rows :
+    ligne = custom_display = Label(main_window, text=r, font=("Helvetica, 12"))
+    ligne.pack()
+
+main_window.mainloop()
